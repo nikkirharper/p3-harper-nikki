4cbd6991fe6acdfaa3f3ef58a7f8668294e2414d
Nikki Harper <nicoleharper@ufl.edu>
2021-04-12 19:56:01 -0600

    Add lunch descriptions and images

---
+++ src/modules/lunch-generator.js
@@ -0,0 +1,26 @@
+const lunchDescripton = [
+    "Turkey Burger with Cheese",
+    "Spaghetti with Vegetable Marinara", 
+    "BBQ Chicken Pizza with Pineapple",
+    "Salmon Poke Bowl", 
+    "Black Bean & Corn Quesadilla", 
+    "Shrimp Caesar Salad", 
+    "Italian Sandwich on fresh Baguette", 
+    "Tomato Soup with Fresh Basil",
+    "Dragon Sushi Roll",
+    "Chicken Street Tacos",
+    ];
+
+
+const lunchImage = [
+	"../images/burger.jpg",
+	"../images/pasta.jpg",
+	"../images/pizza.jpg",
+    "../images/poke.jpg",
+    "../images/quesadilla.jpg",
+    "../images/salad.jpg",
+    "../images/sandwich.jpg",
+    "../images/soup.jpg",
+    "../images/sushi.jpg",
+    "../images/tacos.jpg",
+	],
